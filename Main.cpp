20e5959b8a7bdd7f5e72d72146dbb5673825c88d
GraysonHerbert <65082415+GraysonHerbert@users.noreply.github.com>
2020-09-02 18:28:03 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,13 @@
+//Program Title: The Dollar Game
+//File Name: Main.cpp
+//External Files: N/A
+//Names: Grayson Herbert, Jacob Esswein
+//Email: gdhxnw@umsystem.edu, TODO
+//Course: 4250-001
+//Date: TBD
+//Description: A game to play including a graph and stacks of dollars. The goal is to get every stack with positive dollars.
+//Resources: N/A
+
 #include <iostream>
 
 int main(){
